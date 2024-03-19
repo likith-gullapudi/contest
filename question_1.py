e56ab94d0e9821ca118bf0597050887f5dfa8ff5
likith-gullapudi <92703188+likith-gullapudi@users.noreply.github.com>
2024-03-19 22:27:10 +0530

    Add files via upload

--- question_1.py
+++ question_1.py
@@ -1,32 +1,32 @@
 class MyQueue:
 
     def __init__(self):
-        self.a = []  
-        self.b = []  
+        self.queue1 = []
+        self.queue2 = []  
 
     def push(self, x: int) -> None:
-        if self.b == []:
-            self.a, self.b = self.b, self.a
-        if self.a == []:
-            while self.b:
-                self.a.append(self.b.pop(-1))
-            self.a.append(x)
-            while self.a:
-                self.b.append(self.a.pop(-1))
+        if self.queue2 == []:
+            self.queue1, self.queue2 = self.queue2, self.queue1
+        if self.queue1 == []:
+            while self.queue2:
+                self.queue1.append(self.queue2.pop(-1))
+            self.queue1.append(x)
+            while self.queue1:
+                self.queue2.append(self.queue1.pop(-1))
 
     def pop(self) -> int:
-        if self.b == []:
-            self.a, self.b = self.b, self.a
-        return self.b.pop(-1)
+        if self.queue2 == []:
+            self.queue1, self.queue2 = self.queue2, self.queue1
+        return self.queue2.pop(-1)
 
     def peek(self) -> int:
-        if self.b == []:
-            self.a, self.b = self.b, self.a
-
-        return self.b[-1]
+        if self.queue2 == []:
+            self.queue1, self.queue2 = self.queue2, self.queue1
+        return self.queue2[-1]
 
     def empty(self) -> bool:
-        return self.a == [] and self.b == []
+        return self.queue1 == [] and self.queue2 == []
+
 
 # Your MyQueue object will be instantiated and called as such:
 # obj = MyQueue()
